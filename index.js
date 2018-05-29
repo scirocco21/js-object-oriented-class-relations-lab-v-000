a1e57de5ca6ab4e593d69850118d295d99eb8c20
Seb Gertz <sebgertz@gmail.com>
2018-05-29 00:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,11 +35,13 @@ class Trip {
     this.passengerId = passenger.id;
     this.id = ++tripId;
     store.trips.push(this);
+
     if (driver) {
       driver.tripsArray.push(this);
-    }
+    };
     if (passenger) {
       passenger.tripsArray.push(this);
+    }
   }
   driver() {
     return store.drivers.find(driver => {
