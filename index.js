f917f536da4670a58d50746f0c9ff342f5bafa28
Seb Gertz <sebgertz@gmail.com>
2018-05-29 00:53:36 +0000

    rough working solution

---
+++ index.js
@@ -0,0 +1,88 @@
+let store = {drivers: [], passengers: [], trips: []};
+let driverId = 0;
+
+class Driver {
+  constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+      this.tripsArray = [];
+      store.drivers.push(this);
+  }
+
+  trips() {
+    return this.tripsArray
+  }
+  passengers() {
+    let passengersIds = [];
+    let passengers = [];
+
+    this.tripsArray.forEach(trip => {
+      passengersIds.push(trip.passengerId);
+    })
+
+    store.passengers.forEach(passenger => {
+      if (passengersIds.includes(passenger.id)) {
+        passengers.push(passenger)
+      }
+    })
+  return passengers;
+  }
+}
+
+let passengerId = 0;
+
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = ++passengerId;
+    this.tripsArray = [];
+    store.passengers.push(this);
+  }
+  trips() {
+    return this.tripsArray;
+  }
+
+  drivers() {
+    let driversIds = [];
+    let drivers = [];
+
+    this.tripsArray.forEach(trip => {
+      driversIds.push(trip.driverId);
+    })
+
+    store.drivers.forEach(driver => {
+      if (driversIds.includes(driver.id)) {
+        drivers.push(driver)
+      }
+    })
+    return drivers;
+}
+
+}
+
+let tripId = 0;
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    this.id = ++tripId;
+    store.trips.push(this);
+
+    if (driver) {
+      driver.tripsArray.push(this);
+    }
+    if (passenger) {
+      passenger.tripsArray.push(this);
+    }
+  }
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    })
+  }
+  passenger() {
+     return store.passengers.find(passenger => {
+       return passenger.id === this.passengerId;
+     })
+  }
+}
